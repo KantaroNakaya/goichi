bef0ee54115f81926ebf0ea4731e2d7af5eace94
Kantaro Nakaya <shimeno38saiko@gmail.com>
2024-04-29 11:18:35 +0900

    smoothTextクラスが滑らかに出現する動きの実装

--- js/script.js
+++ js/script.js
@@ -1,60 +1,80 @@
 //=====画面読込時のjquery
-$(window).on('load',function(){
-$("#splash-logo").delay(0).fadeOut('slow');
+$(window).on("load", function () {
+  $("#splash-logo").delay(0).fadeOut("slow");
 
-//=====ここからローディングエリア（splashエリア）をフェードアウトした後に動かしたいJSをまとめる
-$("#splash").delay(10).fadeOut('slow',function(){
-
-$('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
-
-});
-//=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-
+  //=====ここからローディングエリア（splashエリア）をフェードアウトした後に動かしたいJSをまとめる
+  $("#splash")
+    .delay(10)
+    .fadeOut("slow", function () {
+      $("body").addClass("appear"); //フェードアウト後bodyにappearクラス付与
+    });
+  //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
 });
 
 //=====メインビジュアルのスライド
-$('.slider').slick({
-		fade:true,//切り替えをフェードで行う。初期値はfalse。
-		autoplay: true,//自動的に動き出すか。初期値はfalse。
-		autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
-		speed:1000,//スライドの動きのスピード。初期値は300。
-		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-		slidesToShow: 1,//スライドを画面に3枚見せる
-		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-		arrows: false,
-		dots: false,
-        pauseOnFocus: false,//フォーカスで一時停止を無効
-        pauseOnHover: false,//マウスホバーで一時停止を無効
-        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+$(".slider").slick({
+  fade: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 1000,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  pauseOnFocus: false,
+  pauseOnHover: false,
+  pauseOnDotsHover: false,
 });
-$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
-    $('.slider').slick('slickPlay');
+$(".slider").on("touchmove", function (event, slick, currentSlide, nextSlide) {
+  $(".slider").slick("slickPlay");
 });
 
 //=====ハンバーガーメニュー部分
-$(".openbtn").click(function () {//ボタンがクリックされたら
-	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+$(".openbtn").click(function () {
+  $(this).toggleClass("active");
+  $("#g-nav").toggleClass("panelactive");
 });
 
-$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+$("#g-nav a").click(function () {
+  //ナビゲーションのリンクがクリックされたら
+  $(".openbtn").removeClass("active");
+  $("#g-nav").removeClass("panelactive");
 });
 
 // 「ページトップに戻る」ボタンの実装
-var pagetop = $('#js-pagetop');
-	pagetop.hide();
-	$(window).scroll(function () {
-	if ($(this).scrollTop() > 500) {
-		pagetop.fadeIn();
-	} else {
-		pagetop.fadeOut();
-	}
-	});
-	pagetop.click(function () {
-	$('body, html').animate({
-		scrollTop: 0
-	}, 500);
-	return false;
-	});
+var pagetop = $("#js-pagetop");
+pagetop.hide();
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 500) {
+    pagetop.fadeIn();
+  } else {
+    pagetop.fadeOut();
+  }
+});
+pagetop.click(function () {
+  $("body, html").animate(
+    {
+      scrollTop: 0,
+    },
+    500
+  );
+  return false;
+});
+
+// smoothTextクラスが滑らかに出現する動き
+function SmoothTextAnime() {
+  $(".smoothTextTrigger").each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("smoothTextAppear");
+    } else {
+      $(this).removeClass("smoothTextAppear");
+    }
+  });
+}
+$(window).scroll(function () {
+  SmoothTextAnime();
+});
